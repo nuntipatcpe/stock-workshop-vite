a2cec84f10aa7614cfc7c37865478cc03d5a3ea0
boom <nuntipatcpe@gmail.com>
2022-08-23 13:13:07 +0700

    feat : Herder Menu

---
+++ README.md
@@ -0,0 +1,77 @@
+- typescript
+
+# VS code extention
+
+```
+code --install-extension dsznajder.es7-react-js-snippets
+code --install-extension vscode-icons-team.vscode-icons
+code --install-extension naumovs.color-highlight
+code --install-extension esbenp.prettier-vscode
+code --install-extension humao.rest-client
+code --install-extension riazxrazor.html-to-jsx
+code --install-extension christian-kohler.path-intellisense
+code --install-extension alexcvzz.vscode-sqlite
+```
+
+# Mui
+
+```
+npm install @mui/material @emotion/react @emotion/styled
+```
+
+hotkey
+
+- control + spacbar
+
+Unit
+
+- 1point = 8px
+
+# Dependencies :
+
+- @emotion/react 11.10.0
+- @emotion/styled 11.10.0
+- @mui/material 5.10.1
+- @mui/icons-material 5.8.4
+- @mui/x-data-grid 5.15.3
+- @react-hook/debounce 4.0.0
+- @types/react-router-dom 5.3.3
+- @types/redux-logger 3.0.9
+- @types/url-join 4.0.1
+- axios 0.27.2
+- chart.js 3.9.1
+- formik 2.2.9
+- formik-material-ui 4.0.0-alpha.2 deprecated
+- moment 2.29.4
+- react-chartjs-2 4.3.1
+- react-iframe 1.8.0
+- react-moment 1.1.2
+- react-number-format 4.9.3
+- react-redux 8.0.2
+- react-router-dom 6.3.0
+- redux 4.2.0
+- redux-logger 3.0.6
+- redux-thunk 2.4.1
+- url-join 5.0.0
+
+```
+pnpm i @emotion/react @emotion/styled @mui/icons-material @mui/material @mui/x-data-grid chart.js react-chartjs-2 @react-hook/debounce react-router-dom @types/react-router-dom axios formik formik-material-ui moment react-moment url-join react-number-format @types/redux-logger react-redux redux redux-logger redux-thunk url-join @types/url-join react-iframe
+```
+
+- create-react-component-folder ^0.3.7 **NPX only**
+
+  ```
+  pnpm i -D create-react-component-folder
+  ```
+
+  cd src/components/pages
+
+  ```
+  npx crcf -f --notest --typescript LoginPage RegisterPage ReportPage StockPage StockCreatePage StockEditPage AboutUs
+  ```
+
+  cd src/components/layouts
+
+  ```
+  npx crcf -f --notest --typescript Header Menu
+  ```
--- src/components/layouts/Header/Header.tsx
+++ src/components/layouts/Header/Header.tsx
@@ -18,16 +18,6 @@ import ListItemIcon from "@mui/material/ListItemIcon";
 import ListItemText from "@mui/material/ListItemText";
 import InboxIcon from "@mui/icons-material/MoveToInbox";
 import MailIcon from "@mui/icons-material/Mail";
-import Header from "./components/layouts/Header";
-import Menu from "./components/layouts/Menu";
-import { Link, Navigate, Route, Routes } from "react-router-dom";
-import LoginPage from "./components/pages/LoginPage";
-import RegisterPage from "./components/pages/RegisterPage";
-import StockPage from "./components/pages/StockPage";
-import StockCreatePage from "./components/pages/StockCreatePage";
-import StockEditPage from "./components/pages/StockEditPage";
-import ReportPage from "./components/pages/ReportPage";
-import AboutUs from "./components/pages/AboutUs";
 
 const drawerWidth = 240;
 
@@ -80,44 +70,35 @@ const DrawerHeader = styled("div")(({ theme }) => ({
   justifyContent: "flex-end",
 }));
 
-export default function App() {
+type HeaderProp = {
+  open: boolean;
+  notSent?: boolean; // ? sent or not sent
+  onDrawerOpen: () => void;
+};
+
+export default function Header({ open, onDrawerOpen }: HeaderProp) {
   const theme = useTheme();
-  const [open, setOpen] = React.useState(true);
 
   const handleDrawerOpen = () => {
-    setOpen(true);
-  };
-
-  const handleDrawerClose = () => {
-    setOpen(false);
+    onDrawerOpen();
   };
 
   return (
-    <Box sx={{ display: "flex" }}>
-      <CssBaseline />
-      <Header open={open} onDrawerOpen={handleDrawerOpen} />
-      <Menu open={open} onDrawerClose={handleDrawerClose} />
-      <Main open={open}>
-        <DrawerHeader />
-        <Routes>
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/register" element={<RegisterPage />} />
-          <Route path="/stock" element={<StockPage />} />
-          <Route path="/stock/create" element={<StockCreatePage />} />
-          <Route path="/stock/edit/:id" element={<StockEditPage />} />
-          <Route path="/report" element={<ReportPage />} />
-          <Route path="/aboutus" element={<AboutUs />} />
-          <Route path="/" element={<Navigate to="/login" />} />
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </Main>
-    </Box>
+    <AppBar position="fixed" open={open}>
+      <Toolbar>
+        <IconButton
+          color="inherit"
+          aria-label="open drawer"
+          onClick={handleDrawerOpen}
+          edge="start"
+          sx={{ mr: 2, ...(open && { display: "none" }) }}
+        >
+          <MenuIcon />
+        </IconButton>
+        <Typography variant="h6" noWrap component="div">
+          Persistent drawer
+        </Typography>
+      </Toolbar>
+    </AppBar>
   );
 }
-
-const NotFound = () => (
-  <div>
-    <h1>404| Not Found!</h1>
-    <Link to="/">Home</Link>
-  </div>
-);
