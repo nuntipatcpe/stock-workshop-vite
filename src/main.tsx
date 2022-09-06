956f6d0850c41dff47c788d1dccf3c722b74071c
boom <nuntipatcpe@gmail.com>
2022-09-06 13:16:14 +0700

    refactor: main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -4,21 +4,8 @@ import "./index.css";
 
 import { BrowserRouter } from "react-router-dom";
 
-import { createStore, applyMiddleware, Middleware } from "redux";
-import thunk from "redux-thunk";
-
-import { Provider, useDispatch } from "react-redux";
+import { Provider } from "react-redux";
 import { store } from "./Redux/reducers";
-
-// import { store } from "./Redux/store";
-// import reducers from "./Redux/reducers";
-// import logger from "redux-logger";
-// let middleware: Middleware[] = [thunk];
-// if (import.meta.env.VITE_IS_PRODUCTION === "0") {
-//   console.log(import.meta.env.VITE_IS_PRODUCTION);
-//   middleware.push(logger);
-// }
-// export const store = createStore(reducers, applyMiddleware(...middleware));
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <BrowserRouter>
@@ -27,6 +14,3 @@ ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
     </Provider>
   </BrowserRouter>
 );
-
-// export type AppDispatch = typeof store.dispatch;
-// export const useAppDispatch = () => useDispatch<AppDispatch>();
